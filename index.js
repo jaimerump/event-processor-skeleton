4516e748edbe61723a8edb685d82d63ae6629730
Jaime Rump <jaime.lee.rump@gmail.com>
2017-07-14 14:42:05 -0400

    Extract queue-wrapper into its own package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
   Head: require('./lib/head'),
   Pipeline: require('./lib/pipeline'),
-  Stage: require('./lib/stage'),
-  Wrapper: require('./lib/queue-wrapper'),
+  Stage: require('./lib/stage')
 }
